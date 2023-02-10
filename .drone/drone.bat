640d75e49a961619009505a7fe7539aef3ed7541
Klemens Morgenstern <klemens.d.morgenstern@gmail.com>
2023-02-10 19:00:15 +0800

    Copied beast build script.

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -1,14 +1,10 @@
+
 @ECHO ON
 setlocal enabledelayedexpansion
 
 if "%DRONE_JOB_BUILDTYPE%" == "boost" (
 
-echo '==================================> INSTALL'
-
-git clone https://github.com/boostorg/boost-ci.git boost-ci-cloned --depth 1
-cp -prf boost-ci-cloned/ci .
-rm -rf boost-ci-cloned
-
+echo "============> INSTALL"
 
 SET DRONE_BUILD_DIR=%CD: =%
 choco install --no-progress -y openssl --x64 --version 1.1.1.1000
@@ -16,29 +12,26 @@ mklink /D "C:\OpenSSL" "C:\Program Files\OpenSSL-Win64"
 SET OPENSSL_ROOT=C:\OpenSSL
 SET BOOST_BRANCH=develop
 IF "%DRONE_BRANCH%" == "master" SET BOOST_BRANCH=master
-
-REM source ci/travis/install.sh
-REM The contents of install.sh below:
-
-for /F %%i in ("%DRONE_REPO%") do @set SELF=%%~nxi
-SET BOOST_CI_TARGET_BRANCH=%DRONE_COMMIT_BRANCH%
-SET BOOST_CI_SRC_FOLDER=%cd%
-
-call ci\common_install.bat
-
-echo '==================================> COMPILE'
-
-REM set B2_TARGETS=libs/!SELF!/test libs/!SELF!/example
-set B2_TARGETS=libs/!SELF!/test
-
-cd !BOOST_ROOT!
+cp tools\user-config.jam %USERPROFILE%\user-config.jam
+cd ..
+SET GET_BOOST=!DRONE_BUILD_DIR!\tools\get-boost.sh
+bash -c "$GET_BOOST $DRONE_BRANCH $DRONE_BUILD_DIR"
+cd boost-root
 call bootstrap.bat
 b2 headers
-b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% --verbose-test libs/!SELF!/test -j3
-b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% --verbose-test libs/!SELF!/example -j3
 
-) else if "%DRONE_JOB_BUILDTYPE%" == "standalone-windows" (
+echo "============> SCRIPT"
 
-REM not used
+IF DEFINED DEFINE SET B2_DEFINE="define=%DEFINE%"
+
+echo "Running tests"
+b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% %B2_DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% --verbose-test libs/requests/test -j3
+if !errorlevel! neq 0 exit /b !errorlevel!
+
+echo "Running libs/requests/example"
+b2 --debug-configuration variant=%VARIANT% cxxstd=%CXXSTD% %B2_DEFINE% address-model=%ADDRESS_MODEL% toolset=%TOOLSET% libs/requests/example -j3
+if !errorlevel! neq 0 exit /b !errorlevel!
+
+echo "============> COMPLETED"
 
 )
